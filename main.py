152803cf5b3a41c93989103ee13ef471f6c88446
jahid-unlock <jahid.nits@gmail.com>
2023-12-15 19:24:28 +0600

    debug script added

--- main.py
+++ main.py
@@ -139,6 +139,9 @@ def internal_error(error):
 
 
 if __name__ == "__main__":
+    app.run(debug=True)
+
+if __name__ == "__main__":
     # use 0.0.0.0 for replit hosting
     app.run(host="0.0.0.0", port=8080)
 
